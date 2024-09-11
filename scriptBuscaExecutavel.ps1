3110aea3b6ca969fecade888ddb50fcf1cb289fd
Vinicius Tarcisio Ferreira <tfvinicius@gmail.com>
2024-09-11 20:16:35 -0300

    Implementação final
    Implementado funcionalidade de gerar relatório via HTML, JSON e CSV por meio da passagem de parâmetro ao executar script.

--- scriptBuscaExecutavel.ps1
+++ scriptBuscaExecutavel.ps1
@@ -1,4 +1,4 @@
-
+param($tipoDeExportacao)
 # Aqui criamos um hashtable para a coluna de nome
 $nameExpr = @{
   Label = "Nome";
@@ -12,6 +12,31 @@ $lengthExpr = @{
 
 $params = $nameExpr, $lengthExpr
 
+$resultado = 
 gci -Recurse -File |
   ? Name -Like "*.exe" |
   select $params
+
+
+if ($tipoDeExportacao -eq "HTML") {
+  <# Action to perform if the condition is true #>
+  $estilos = Get-Content styles.css
+  #$styleTag = "<style>" + $estilos + "</style>"
+  $styleTag = "<style> $estilos </style>" 
+  $tituloPagina = "Relatorio de Executáveis"
+  $tituloBody = "<h1> $tituloPagina </h1>"
+
+  $resultado | 
+      ConvertTo-Html -Head $styleTag -Title $tituloPagina -Body $tituloBody|
+      Out-File relatorio.html
+} elseif ($tipoDeExportacao -eq "JSON") {
+  $resultado |
+    ConvertTo-Json |
+    Out-File relatorio.json
+  <# Action when this condition is true #>
+} elseif ($tipoDeExportacao -eq "CSV") {
+  $resultado |
+    ConvertTo-Csv -NoTypeInformation -Delimiter "1"|
+    Out-File relatorio.csv
+  <# Action when this condition is true #>
+}
